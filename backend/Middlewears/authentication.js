b68df6e1842fa27bfa63bc6d0618a520ff6252a0
AhmadQadourah <ahmad.h.qadoura@gmail.com>
2022-09-26 13:13:37 +0300

    authentication Finish

---
+++ backend/Middlewears/authentication.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+require("dotenv").config()
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
+
+
+const authentication =(req ,res ,next)=>{
+
+    try{
+        if (!req.body.authorization){
+            return res.status(403).json({
+                success: false,
+                message: `Forbidden`,
+              });
+        }
+        const  token =  req.headers.authorization.split(" ").pop()
+        jwt.verify(token ,SECRET , (err,result)=>{
+            if (err){
+                res.status(403).json({
+                    success: false,
+                    message: `The token is invalid or expired`,
+                  });
+            }
+
+            else {
+                req.token=result
+                   next()
+            }
+        
+        })
+
+    }
+    catch (err){
+        res.status(500).json({
+            success: false,
+            message: `Server Error`,
+            err: err.message,
+          });
+    }
+
+}
+
+module.exports = authentication;
